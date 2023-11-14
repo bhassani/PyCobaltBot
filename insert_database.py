684dd649b0cac27ae8556e7d9cf3a5c0d23fdf2f
bhassani <bhassani@users.noreply.github.com>
2023-11-14 16:06:19 +0100

    Update insert_database.py

--- insert_database.py
+++ insert_database.py
@@ -1,6 +1,29 @@
 from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
+import pymysql 
+
+#https://www.geeksforgeeks.org/python-mariadb-insert-into-table-using-pymysql/
+def mysql_insert(name):
+    Host = "localhost"  
+    # User name of the database server 
+    User = "user"       
+    # Password for the database user 
+    Password = ""            
+      
+    database = "database_name"
+      
+    conn  = pymysql.connect(host=Host, user=User, password=Password, database) 
+      
+    # Create a cursor object 
+    cur  = conn.cursor() 
+    
+    query = f"INSERT INTO USERS (username) VALUES ('{name}')"
+    cur.execute(query) 
+    print(f"{cur.rowcount} details inserted") 
+    conn.commit() 
+    conn.close() 
+
 #https://www.mysqltutorial.org/python-mysql-insert/
 def insert_record(name):
     query = "INSERT INTO users(username) " \
